a3bb92e91ff964c9c82dea6bd89e040cfd3a9b99
rodjean015 <rodjeanverzosa001@gmail.com>
2026-04-27 15:16:34 +0800

    revise grid line

--- components/ui/BackgroundGrid.tsx
+++ components/ui/BackgroundGrid.tsx
@@ -5,7 +5,7 @@ type BackgroundGridProps = {
 
 export default function BackgroundGrid({
     dark,
-    size = 60,
+    size = 30,
 }: BackgroundGridProps) {
     return (
         <div className="pointer-events-none absolute inset-0 z-0 overflow-hidden">
@@ -13,8 +13,8 @@ export default function BackgroundGrid({
                 className={`
                     absolute inset-0
                     ${dark
-                        ? "bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)] opacity-40"
-                        : "bg-[linear-gradient(to_right,#e5e5e5_1px,transparent_1px),linear-gradient(to_bottom,#e5e5e5_1px,transparent_1px)] opacity-40"
+                        ? "bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)] opacity-20"
+                        : "bg-[linear-gradient(to_right,#e5e5e5_1px,transparent_1px),linear-gradient(to_bottom,#e5e5e5_1px,transparent_1px)] opacity-20"
                     }
                 `}
                 style={{
